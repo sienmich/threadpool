b278841f79235df0412a1e80b4598e71509d92b4
sienmich <sienmich@gmail.com>
2020-01-05 11:58:28 +0100

    removed waiting from map

--- future.c
+++ future.c
@@ -1,9 +1,10 @@
+#include <stdlib.h>
 #include "future.h"
 #include "threadpool.h"
 #include "runnable.h"
 #include "err.h"
 
-void function(void *args, size_t argsz __attribute__((unused))) {
+void eval_future(void *args, size_t argsz __attribute__((unused))) {
     future_t *future = args;
 
     try(pthread_mutex_lock(&future->mutex));
@@ -22,17 +23,38 @@ int async(thread_pool_t *pool, future_t *future, callable_t callable) {
     try(pthread_mutex_init(&future->mutex, 0));
     try(pthread_cond_init(&future->for_res, 0));
 
-
     try(defer(pool, (runnable_t)
-            {.arg = future, .argsz = sizeof(future_t *), .function = function}));
+            {.arg = future, .argsz = sizeof(future_t *), .function = eval_future}));
 
     return 0;
 }
 
+struct map_args {
+    future_t *from;
+    void *(*function)(void *, size_t, size_t *);
+};
+
+void* eval_map(void *args, size_t argsz, size_t* ressize) {
+    struct map_args a = *(struct map_args *)args;
+
+    void *res = a.function(await(a.from), argsz, ressize);
+
+    free(args);
+
+    return res;
+}
+
 int map(thread_pool_t *pool, future_t *future, future_t *from,
         void *(*function)(void *, size_t, size_t *)) {
-    try(async(pool, future, (callable_t) {.arg = await(
-            from), .argsz = from->res_size, .function = function}));
+
+    struct map_args *args;
+    args = safe_malloc(sizeof(struct map_args));
+    args->from = from;
+    args->function = function;
+
+
+    try(async(pool, future, (callable_t) {.arg = args,
+              .argsz = sizeof(&args), .function = eval_map}));
 
     return 0;
 }
